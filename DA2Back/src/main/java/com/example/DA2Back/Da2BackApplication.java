df356f15605ed53f4e3a61dd1b39537d038302ab
MaximoSarmiento2003 <maximo20032003@gmail.com>
2026-08-23 14:45:01 -0300

    Agregar backend Spring Boot

---
+++ DA2Back/src/main/java/com/example/DA2Back/Da2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.DA2Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Da2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Da2BackApplication.class, args);
+	}
+
+}
---
+++ DA2Back/src/test/java/com/example/DA2Back/Da2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DA2Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Da2BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
